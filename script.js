daa28a9d7d5f8f399f7726103d1571e775d17550
Genrev Gabriel C. Rodriguez <genrevgrodriguez@gmail.com>
2025-02-11 01:12:34 +0800

    Initial commit

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
-$(document).ready(function(){
-    $(".container").mouseenter(function(){
-        $(".card").stop().animate({
-            top:"-90px"
-        }, "slow");
-    }).mouseleave(function(){
-        $(".card").stop().animate({
-            top:0
-        }, "slow");
-    });
-});
+let leftPos = 0
+let bottomPos = 0
+let noButton = document.getElementById('nooo');
+document.getElementById('yessir').addEventListener('click', () => {
+    document.getElementById('invitation').style.display = 'none';
+    document.getElementById('confirmation').style.display = null;
+})
+
+setInterval(function () {
+    if (noButton.matches(':hover')) {
+        requestAnimationFrame(moveNoButton());
+    }
+}, 100);
+
+noButton.addEventListener('click', () => {
+    while (noButton.matches(':hover')) {
+        requestAnimationFrame(moveNoButton());
+    }
+})
+noButton.addEventListener('touch', () => {
+    moveNoButton()
+})
+
+function moveNoButton() {
+    leftPos = Math.floor(Math.random() * (50 - (-50) + 1)) + (-50);
+    bottomPos = Math.floor(Math.random() * (100 - (-100) + 1)) + (-100);
+    noButton.style.position = "absolute"
+    noButton.style.left = leftPos + "%"
+    noButton.style.bottom = bottomPos + "%"
+}
